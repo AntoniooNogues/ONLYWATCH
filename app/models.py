9dceddec0bd706b03a36b1a424d6afe067a85d7b
Antonio Nogues <anoguesgomez@safareyes.es>
2024-04-02 17:20:01 +0200

    models.py --> add usuario, serie, plataforma

--- app/models.py
+++ app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class plataforma(models.Model):
+    nombre = models.CharField(max_length=50)
+    img = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.nombre+" "+self.img
+
+class usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    tipo = models.IntegerField()
+    img = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.nombre+" "+self.img
+
+class serie(models.Model):
+    nombre = models.CharField(max_length=100)
+    sinopsis = models.TextField()
+    img = models.CharField(max_length=300)
+    trailer = models.CharField(max_length=300)
+    director = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre+" "+self.img+" "+self.trailer+" "+self.director
