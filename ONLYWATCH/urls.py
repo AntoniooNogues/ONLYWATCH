2786a5b229030b1a9063afb45774a9aecd8a0917
Antonio Nogues <anoguesgomez@safareyes.es>
2024-04-05 17:24:11 +0200

    vista de plataforma y url de la vista

--- ONLYWATCH/urls.py
+++ ONLYWATCH/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('crear_plataforma/', views.crear_plataforma),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from static.img import *
+import os
+from django.core.files import File
+from .models import plataforma
+from django.http import HttpResponse
 
-# Create your views here.
+def crear_plataforma(request):
+    img_dir = 'static/img'  # directory where images are stored
+    for filename in os.listdir(img_dir):
+        if filename.endswith(".svg"):  # check if the file is an image
+            img_path = os.path.join(img_dir, filename)
+            with open(img_path, 'rb') as img_file:
+                crea = plataforma(nombre=os.path.splitext(filename)[0], img=File(img_file))
+                crea.save()
+    return HttpResponse("Plataformas creadas")
