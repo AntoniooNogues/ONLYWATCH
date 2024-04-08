106f71c8cfee8b06338eeb994d5248c60b0564f5
Antonio Nogues <anoguesgomez@safareyes.es>
2024-04-08 19:22:41 +0200

    añadir colores a la configuracion de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
   content: ["./templates/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      c1: "#FFF5ED",
+      c2: "#FFE9D5",
+      c3: "#FECFAA",
+      c4: "#FECFAA",
+      c5: "#FDAC74",
+      c6: "#FB752D",
+      c7: "#F95C16",
+      c8: "#EA420C",
+      c9: "#C22F0C",
+      c10: "#9A2612",
+      c11: "#7C2212",
+      c12: "#430E07",
+    }
   },
   plugins: [],
 }
