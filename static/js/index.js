c83ad82068f7e630011064bf71b9f091b176956d
Antonio Nogues <anoguesgomez@safareyes.es>
2024-04-11 17:35:41 +0200

    añadir funcionalidad esconder/ver contraseña (modificar el como se ve)

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,18 @@
-import { createRoot } from 'react-dom/client';
+document.getElementById("togglePasswordVisibility").addEventListener("click", function(event) {
+    event.stopPropagation();
+    togglePasswordVisibility();
+});
 
-const root = createRoot(document.getElementById('app'));
-root.render();
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById("password");
+    var eyeIcon = document.querySelector("#togglePasswordVisibility i");
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        eyeIcon.classList.remove("fa-eye");
+        eyeIcon.classList.add("fa-eye-slash");
+    } else {
+        passwordInput.type = "password";
+        eyeIcon.classList.remove("fa-eye-slash");
+        eyeIcon.classList.add("fa-eye");
+    }
+}
