89b4049d958dd2d7a318518ee4675c1f08641021
Antonio Nogues <anoguesgomez@safareyes.es>
2024-04-22 19:36:48 +0200

    actualizar tabla usuario

--- app/models.py
+++ app/models.py
@@ -11,11 +11,11 @@ class plataforma(models.Model):
 
 class Rol(models.TextChoices):
     ADMINISTRADOR = 'ADMIN', 'Administrador'
-    USUARIO = 'USUARIO', 'Usuario'
+    USUARIO = 'USER', 'user'
 
 
 class UserManager(BaseUserManager):
-    def create_user(self,  email, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('El usuario debe tener un email')
         email = self.normalize_email(email)
@@ -31,8 +31,6 @@ class UserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser debe tener is_superuser=True.')
         return self.create_user(email, password, **extra_fields)
-
-
 
 class User(AbstractBaseUser):
     username = models.CharField(max_length=50, unique=True)
