dc28f7d3113681d3e5562a5c5cc83ff4a6958227
Antonio Nogues <anoguesgomez@safareyes.es>
2024-04-22 18:50:03 +0200

    arreglar boton mostrar contraseña

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,6 @@
-document.getElementById("togglePasswordVisibility").addEventListener("click", function(event) {
-    event.stopPropagation();
-    togglePasswordVisibility();
-});
-
-function togglePasswordVisibility() {
-    var passwordInput = document.getElementById("password");
-    var eyeIcon = document.querySelector("#togglePasswordVisibility i");
+function togglePasswordVisibility(passwordId, buttonId) {
+    var passwordInput = document.getElementById(passwordId);
+    var eyeIcon = document.querySelector("#" + buttonId + " i");
     if (passwordInput.type === "password") {
         passwordInput.type = "text";
         eyeIcon.classList.remove("fa-eye");
@@ -15,4 +10,20 @@ function togglePasswordVisibility() {
         eyeIcon.classList.remove("fa-eye-slash");
         eyeIcon.classList.add("fa-eye");
     }
+}
+
+var togglePasswordVisibilityButton1 = document.getElementById("togglePasswordVisibility1");
+if (togglePasswordVisibilityButton1) {
+    togglePasswordVisibilityButton1.addEventListener("click", function(event) {
+        event.stopPropagation();
+        togglePasswordVisibility("password1", "togglePasswordVisibility1");
+    });
+}
+
+var togglePasswordVisibilityButton2 = document.getElementById("togglePasswordVisibility2");
+if (togglePasswordVisibilityButton2) {
+    togglePasswordVisibilityButton2.addEventListener("click", function(event) {
+        event.stopPropagation();
+        togglePasswordVisibility("password2", "togglePasswordVisibility2");
+    });
 }
