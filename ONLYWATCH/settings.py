6c5fd6b3d8ff85da8dd8c80da06f0cb436af3c9a
Rodrigo <rjaencobos@safareyes.es>
2024-04-27 23:59:51 +0200

    Mejora de la seguridad, creacion del archivo credenciales.env
    Signed-off-by: Rodrigo <rjaencobos@safareyes.es>

--- ONLYWATCH/settings.py
+++ ONLYWATCH/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -135,9 +136,13 @@ AUTHENTICATION_BACKENDS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Construye la ruta al archivo .env
+env_path = BASE_DIR / 'credenciales.env'
+load_dotenv(dotenv_path=env_path)
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'onlywatch.info@gmail.com'
-EMAIL_HOST_PASSWORD = 'xpzz asxv oywa vgmm'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
